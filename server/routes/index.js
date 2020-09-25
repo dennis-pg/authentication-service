69613af15a9e100b966263a1a01467f7e0302efa
Amit Barletz <amitbarletz@Amits-MBP.lan>
2020-09-25 21:03:47 +0300

    add cookie parser

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 const app = require('@greenpress/api-kit').app()
+const cookieParser = require('cookie-parser')
+
 
 const verifyUser = require('../middleware/verify-user')
 const { onlyAuthenticated } = require('../middleware/auth-check')
 
-app.use(require('cookie-parser')())
+app.use(cookieParser);
 
 app
 	.post('/api/signin', require('../controllers/signin'))
